bfc62cb509ddfe85ec1412c73b0972b172d01ad5
jacktwotwoone <jacktwotwoone@gmail.com>
2025-03-03 07:12:28 +0000

    changeNDK27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,10 @@ plugins {
 android {
     namespace = "com.example.myapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -42,3 +43,8 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+}
+
