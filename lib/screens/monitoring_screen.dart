8febbfddfc75712acc424a83ca602703507d51dc
jacktwotwoone <jacktwotwoone@gmail.com>
2025-03-04 14:10:56 +0000

    monitoringUIcanrun

--- lib/screens/monitoring_screen.dart
+++ lib/screens/monitoring_screen.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-// 远程监控页面
-class MonitoringScreen extends StatelessWidget {
+class MonitoringScreen extends StatefulWidget {
+  @override
+  _MonitoringScreenState createState() => _MonitoringScreenState();
+}
+
+class _MonitoringScreenState extends State<MonitoringScreen> {
+  late VlcPlayerController _vlcController;
+
+  @override
+  void initState() {
+    super.initState();
+    _vlcController = VlcPlayerController.network(
+      'rtsp://admin:1@192.168.41.117:8554/live', // 替换为你的 RTSP 地址
+      hwAcc: HwAcc.full, // 启用硬件加速
+      autoPlay: true, // 自动播放
+      options: VlcPlayerOptions(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _vlcController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.videocam, size: 100, color: Colors.blue),
-            SizedBox(height: 20),
-            Text(
-              '远程监控',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      appBar: AppBar(title: Text('远程监控')),
+      body: Stack(
+        children: [
+          VlcPlayer(
+            controller: _vlcController,
+            aspectRatio: 16 / 9,
+            placeholder: Center(child: CircularProgressIndicator()), // 加载时显示进度条
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: FloatingActionButton(
+              backgroundColor: Colors.blue,
+              onPressed: () => _vlcController.play(), // 重新播放
+              child: Icon(Icons.play_arrow, color: Colors.white),
             ),
-            SizedBox(height: 10),
-            Text(
-              '实时查看监控画面',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
